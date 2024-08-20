d6641a3316e9aaf44d10df80f126a34ec51074e8
camilo <camiloesteban661@gmail.com>
2024-08-20 17:18:01 -0500

    feat: implement user profile fallback image

--- entrypoints/newtab/components/Welcome.tsx
+++ entrypoints/newtab/components/Welcome.tsx
@@ -3,12 +3,11 @@ import { Clock } from "./Clock"
 import useDataStore from "../store/useDataStore"
 
 export const Welcome = () => {
-  // FIX: profile image fallback to a local image
   const { profileImage, username } = useDataStore(state => state);
 
   return (
-    <div className="flex content-center items-center gap-10 w-fit">
-      <Image src={profileImage} className="w-32" />
+    <div className="flex content-center items-center gap-5 w-fit">
+      <Image src={profileImage} fallbackSrc="/placeholders/profile.svg" className="w-32 h-32 object-cover" />
       <div>
         <h1 className="text-4xl font-bold capitalize">Welcome, {username}</h1>
         <p className="text-lg mt-3">
